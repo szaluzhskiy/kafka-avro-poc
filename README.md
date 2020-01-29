a7e3f3dc3040d4fd31a62a2ccbb37cc2c2b3c514
Mikhail_Ivanov <mikhail_ivanov@epam.com>
2020-01-29 17:04:06 +0400

    Spring boot sample implementation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kafka-avro-poc
 poc of usage avro with kafka including schema evolution
+
+### Read more at:
+https://www.confluent.io/blog/schema-registry-avro-in-spring-boot-application-tutorial/
+
+### Run required infrastructure with: 
+docker-compose -f kafka-schema-registry.yml up
--- scripts/register.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-  --data '{ "schema": "{ \"type\": \"record\", \"name\": \"Payment\", \"namespace\": \"com.epam.edp.sr.insuance_contract\", \"fields\": [ { \"name\": \"id\", \"type\": \"string\" }, { \"name\": \"amount\", \"type\": \"double\" } ]}" }' \
-http://localhost:8081/subjects/payment-value/versions
